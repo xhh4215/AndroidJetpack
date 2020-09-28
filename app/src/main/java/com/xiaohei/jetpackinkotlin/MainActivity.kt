72302f3d62fa5daa4d8140fa05277e374214160a
xhh <18734184215@163.com>
2020-09-28 11:46:55 +0800

    添加应用程序栏

--- app/src/main/java/com/xiaohei/jetpackinkotlin/MainActivity.kt
+++ app/src/main/java/com/xiaohei/jetpackinkotlin/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         hideStatusBar()
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
     }
 
@@ -25,12 +26,35 @@ class MainActivity : AppCompatActivity() {
     fun hideStatusBar() {
         // android  4.0 以下使用如下方式进行状态栏的隐藏
         if (Build.VERSION.SDK_INT < 16) {
-            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
         }
         // android 4.1 设置隐藏状态栏 界面标志被清除后（例如，离开 Activity），如果您希望再次隐藏这些栏，
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         //设置内容在状态栏之后 ，可以防止内容的变形
 //        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        actionBar?.hide()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_more -> {
+            Toast.makeText(application, "action_more", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.action_setting -> {
+            Toast.makeText(application, "action_setting", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+
     }
 }
